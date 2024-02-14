c3d925f7b9baaf4352f359c6c088c7dbff24d970
Frank Hardisty <frank.hardisty@gmail.com>
2024-02-14 13:40:18 -0800

    permutation method and test

--- ch_1_arrays_and_strings.py
+++ ch_1_arrays_and_strings.py
@@ -1,14 +1,14 @@
 def is_unique(input: str) -> bool:
     """
-    Determine if all characters in a string are unique. 
+    Determine if all characters in a string are unique.
     Clarification: Is string UTF-8 encoded?
-    Strategy: Use Python Set 
+    Strategy: Use Python Set
 
     Args:
-        input: String to test 
+        input: String to test
 
     Returns:
-        True if there are no repeat characters, False otherwise 
+        True if there are no repeat characters, False otherwise
 
     """
     chars = set()
@@ -26,6 +26,40 @@ def test_is_unique():
     assert is_unique(input_2) == False
 
 
+def is_permutation(str_1: str, str_2: str) -> bool:
+    """
+    Determine if two strings are permutations of each other.
+
+    Clarification:
+        Are uppercase and lowercase distict?
+        Is it OK to use Python's built-in List?
+
+    Strategy:
+        Turn strings into lists, sort, and compare.
+
+    Args:
+        str_1: First string to compare
+        str:2: Second string to compare
+
+    Returns:
+        True if strings contain the same characters, False otherwise
+    """
+
+    if sorted(list(str_1)) == sorted(list(str_2)):
+        return True
+    return False
+
+
+def test_is_permutation():
+    input_1 = "birb"
+    input_2 = "bbir"
+    input_3 = "bird"
+
+    assert is_permutation(input_1, input_2)
+    assert is_permutation(input_1, input_3) == False
+
+
 if __name__ == "__main__":
     test_is_unique()
+    test_is_permutation()
     print("all done!")
