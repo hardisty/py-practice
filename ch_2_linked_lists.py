cf2451a0a5bb54dd6de05e54bd2f1cd31c972946
Frank Hardisty <frank.hardisty@gmail.com>
2024-04-09 15:35:09 -0700

    remove dupes

--- ch_2_linked_lists.py
+++ ch_2_linked_lists.py
@@ -6,17 +6,39 @@ class Node:
     def __str__(self):
         return f"{self.value} -> {self.next}"
     
+    def append_to_tail(self, value: int):
+        end = Node(value)
+        n = self
+        while (n.next is not None):
+            n = n.next
+        n.next = end
+
+
+    
     
     
 
-def remove_dupes(ll):
-    print(ll)
+def remove_dupes(ll: Node):
+    visited = []
+   #n = ll
+    while ll is not None and ll.next is not None:
+        visited.append(ll.value)
+        if ll.next.value in visited:
+            ll.next = ll.next.next
+        ll = ll.next
     return None
 
+def test_remove_dupes():
+    head = Node(3)
+    head.append_to_tail(4)
+    head.append_to_tail(5)
+    head.append_to_tail(3)
+    first_len = len(str(head))
+    print(head)
+    remove_dupes(head)
+    print(head)
+    assert len(str(head)) < first_len
+
+
 if __name__ == "__main__":
-    ll = Node(5)
-    next = Node(6)
-    ll.next = next
-    print(ll)
-    print(ll.next)
-    remove_dupes(ll)
+    test_remove_dupes()
